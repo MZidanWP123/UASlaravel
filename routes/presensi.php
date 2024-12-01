0f558d00b78582e17a3cdfc49a9eaab9bb254241
Danang Jehan <djackgamers@gmail.com>
2024-12-01 11:16:21 +0800

    add feature presensi

---
+++ routes/presensi.php
@@ -0,0 +1,8 @@
+<?php
+
+use App\Http\Controllers\WebView\PresensiController;
+use Illuminate\Support\Facades\Route;
+
+
+
+// Route::get('/presensi', [PresensiController::class, 'index'])->name('presensi.index');
